938f23837963f8d26a31b802fb7aae15c7ce9c4e
chepelcr <chepelcr@outlook.com>
2025-08-14 23:39:04 +0000

    [UPD] USE DB for supabase

--- server/db.ts
+++ server/db.ts
@@ -17,9 +17,26 @@ export const db = drizzle({ client: pool, schema });*/
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+
+const connectionString = process.env.DATABASE_URL
+
 //config({ path: '.env' }); // or .env.local
-const client = postgres(process.env.DATABASE_URL!);
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString, { prepare: false })
 export const db = drizzle({ client });
+
+
+/*import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import { users } from './schema'
+
+
+
+
+const db = drizzle(client);
+
+const allUsers = await db.select().from(users);
+*/
 
 
 /*import { Pool } from 'pg';
